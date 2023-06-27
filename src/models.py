c0b2442bd5c49824d91821bbb85574cc25740bac
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-06-27 19:15:05 +0000

    Finish first version diagram

--- src/models.py
+++ src/models.py
@@ -1,32 +1,48 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(12), nullable=False, unique=True)
+    first_name = Column(String(30), nullable=False)
+    last_name = Column(String(30), nullable=False)
+    email = Column(String, nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = "follower"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(ForeignKey('user.id'))
+    user_to_id = Column(ForeignKey('user.id'))
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey('user.id'))
+
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    post_id = Column(ForeignKey('post.id'), nullable=False)
+    url = Column(String, nullable=False, nullable=False)
+    type = Column(Enum("feed", "story", "reel"))
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    comment = Column(String, nullable=False)
+    post_id = Column(ForeignKey('post.id'), nullable=False)
+    author_id = Column(ForeignKey('user.id'), nullable=False)
+
+
+def to_dict(self):
+    return {}
 
 ## Draw from SQLAlchemy base
 try:
